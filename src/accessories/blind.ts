4c2fe64c22c8bbc84f08ebbcbc2fe569bdcb0e2c
Pierpaolo Follia <pfollia@gmail.com>
2020-02-10 14:14:17 +0100

    Fix the way we handle mid positions for blinds

--- src/accessories/blind.ts
+++ src/accessories/blind.ts
@@ -1,6 +1,6 @@
 import {ComelitAccessory} from "./comelit";
 import {BlindDeviceData, ComelitClient, ObjectStatus} from "../comelit-client";
-import {Categories, Characteristic, CharacteristicEventTypes, Service} from "hap-nodejs";
+import {Callback, Categories, Characteristic, CharacteristicEventTypes, Service} from "hap-nodejs";
 import {HomebridgeAPI} from "../index";
 import {PositionState} from "hap-nodejs/dist/lib/gen/HomeKit";
 import Timeout = NodeJS.Timeout;
@@ -9,10 +9,11 @@ export class Blind extends ComelitAccessory<BlindDeviceData> {
     static readonly OPEN = 100;
     static readonly CLOSED = 0;
 
-    static readonly OPENING_CLOSING_TIME = 35000; // 35 seconds to open approx.
+    static readonly OPENING_CLOSING_TIME = 35000; // 35 seconds to open approx. We should have this in the config
 
     private coveringService: Service;
     private timeout: Timeout;
+    private lastCommandTime: number;
 
     constructor(log: Function, device: BlindDeviceData, name: string, client: ComelitClient) {
         super(log, device, name, client, Categories.WINDOW_COVERING);
@@ -29,23 +30,40 @@ export class Blind extends ComelitAccessory<BlindDeviceData> {
 
         this.coveringService
             .getCharacteristic(Characteristic.TargetPosition)
-            .on(CharacteristicEventTypes.SET, async (state: number, callback: Function) => {
+            .on(CharacteristicEventTypes.SET, async (position: number, callback: Callback) => {
                 try {
+                    if (this.timeout) {
+                        // A timeout was set, this means that we are already opening or closing the blind
+                        // Stop the blind and calculate a rough position
+                        const now = new Date().getTime();
+                        clearTimeout(this.timeout);
+                        this.timeout = null;
+                        await this.client.toggleDeviceStatus(this.device.id, ObjectStatus.OFF); // stop the blind
+                        const diff = Blind.OPENING_CLOSING_TIME - (now - this.lastCommandTime);
+                        const newPosition = 100/ (Blind.OPENING_CLOSING_TIME / diff);
+                        this.coveringService.getCharacteristic(Characteristic.CurrentPosition).updateValue(newPosition);
+                        this.coveringService.getCharacteristic(Characteristic.TargetPosition).updateValue(newPosition);
+                        this.coveringService.getCharacteristic(Characteristic.PositionState).updateValue(PositionState.STOPPED);
+                        callback();
+                        return;
+                    }
+
                     const currentPosition = this.coveringService.getCharacteristic(Characteristic.CurrentPosition).value as number;
-                    const status = state < currentPosition ? ObjectStatus.OFF : ObjectStatus.ON;
-                    const delta = state < currentPosition ? (currentPosition - state) : (state - currentPosition);
-                    if (delta) {
-                        if (this.timeout) {
-                            clearTimeout(this.timeout);
-                            this.timeout = null;
-                            await this.client.toggleDeviceStatus(this.device.id, ObjectStatus.OFF); // stop the blind
-                        }
+                    const status = position < currentPosition ? ObjectStatus.OFF : ObjectStatus.ON;
+                    const delta = currentPosition - position;
+                    this.log(`Setting value for blind ${this.name} to ${position}. Current position is ${currentPosition}. Delta is ${delta}`);
+                    if (delta !== 0) {
                         await this.client.toggleDeviceStatus(this.device.id, status);
+                        this.lastCommandTime = new Date().getTime();
                         this.timeout = setTimeout(async () => {
-                            this.log(`Stopping blind to ${state}%`);
-                            await this.client.toggleDeviceStatus(this.device.id, ObjectStatus.OFF);
-                            this.coveringService.getCharacteristic(Characteristic.CurrentPosition).updateValue(state);
+                            if (position > Blind.CLOSED && position < Blind.OPEN) {
+                                // We stop the blind only for mid positions, otherwise it would stop by itself
+                                this.log(`Stopping blind to ${position}%`);
+                                await this.client.toggleDeviceStatus(this.device.id, ObjectStatus.OFF);
+                            }
+                            this.coveringService.getCharacteristic(Characteristic.CurrentPosition).updateValue(position);
                             this.timeout = null;
+                            this.lastCommandTime = 0;
                         }, Blind.OPENING_CLOSING_TIME * delta / 100);
                     }
                     callback();
