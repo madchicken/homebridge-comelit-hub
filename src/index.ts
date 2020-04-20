f2045ab6ef616a383773c6aa01ae52967879f47f
Pierpaolo Follia <pfollia@gmail.com>
2020-04-20 21:13:13 +0200

    avoid dynamic platform (until I understand how it works)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import { ComelitPlatform } from "./comelit-platform";
-import { Homebridge } from "../types";
+import { ComelitPlatform } from './comelit-platform';
+import { Homebridge } from '../types';
 
 export let HomebridgeAPI: Homebridge;
 
 export default function(homebridge: Homebridge) {
   HomebridgeAPI = homebridge;
-  homebridge.registerPlatform(
-    "homebridge-comelit-hub",
-    "Comelit",
-    ComelitPlatform,
-    true
-  );
+  homebridge.registerPlatform('homebridge-comelit-hub', 'Comelit', ComelitPlatform, false);
 }
