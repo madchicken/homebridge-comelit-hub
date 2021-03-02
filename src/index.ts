b6a99299d1af5f53f29614f56bddb9433c95c79c
Pierpaolo Follia <pfollia@gmail.com>
2021-03-02 22:11:26 +0100

    Do not import types from hap-nodejs (#36)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { ComelitPlatform } from './comelit-platform';
-import { API, Formats, Perms, Units, PlatformAccessory } from 'homebridge';
-import { Service, Characteristic } from 'hap-nodejs';
+import { API, Formats, Perms, Units, PlatformAccessory, Characteristic, Service } from 'homebridge';
 import fakegato from 'fakegato-history';
 import { FakegatoService } from './types';
 
@@ -8,9 +7,8 @@ interface ExtraHAPTypes {
   Service: typeof Service;
   Characteristic: typeof Characteristic;
   PlatformAccessory: typeof PlatformAccessory;
-  CurrentPowerConsumption: typeof CurrentPowerConsumption;
-  TotalConsumption: typeof TotalConsumption;
-  PowerMeterService: typeof PowerMeterService;
+  CurrentPowerConsumption: any;
+  TotalConsumption: any;
   FakeGatoHistoryService: FakegatoService;
 }
 
@@ -21,52 +19,8 @@ export const HAP: ExtraHAPTypes = {
   PlatformAccessory: null,
   CurrentPowerConsumption: null,
   TotalConsumption: null,
-  PowerMeterService: null,
   FakeGatoHistoryService: null,
 };
-
-export class CurrentPowerConsumption extends Characteristic {
-  public static readonly UUID: string = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
-
-  constructor() {
-    super('CurrentConsumption', CurrentPowerConsumption.UUID, {
-      format: Formats.UINT16,
-      unit: 'watts' as Units, // ??
-      maxValue: 100000,
-      minValue: 0,
-      minStep: 1,
-      perms: [Perms.PAIRED_READ, Perms.NOTIFY],
-    });
-  }
-}
-HAP.CurrentPowerConsumption = CurrentPowerConsumption;
-
-class TotalConsumption extends Characteristic {
-  public static readonly UUID: string = 'E863F10C-079E-48FF-8F27-9C2605A29F52';
-
-  constructor() {
-    super('TotalConsumption', TotalConsumption.UUID, {
-      format: Formats.FLOAT,
-      unit: 'kWh' as Units, // ??
-      minValue: 0,
-      minStep: 0.001,
-      perms: [Perms.PAIRED_READ, Perms.NOTIFY],
-    });
-  }
-}
-HAP.TotalConsumption = TotalConsumption;
-
-class PowerMeterService extends Service {
-  public static readonly UUID: string = 'D9C50529-BC9A-4324-8E79-E17C85FCAC62';
-
-  public UUID: string;
-  constructor(displayName?: string, subtype?: string) {
-    super(displayName, PowerMeterService.UUID, subtype);
-    this.addCharacteristic(HAP.CurrentPowerConsumption);
-    // this.addCharacteristic(HAP.TotalConsumption);
-  }
-}
-HAP.PowerMeterService = PowerMeterService;
 
 export default function(homebridge: API) {
   HomebridgeAPI = homebridge;
@@ -74,6 +28,35 @@ export default function(homebridge: API) {
   HAP.Characteristic = homebridge.hap.Characteristic;
   HAP.PlatformAccessory = homebridge.platformAccessory;
   HAP.FakeGatoHistoryService = fakegato(homebridge);
+
+  HAP.CurrentPowerConsumption = class CurrentPowerConsumption extends HAP.Characteristic {
+    public static readonly UUID: string = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
+
+    constructor() {
+      super('CurrentConsumption', CurrentPowerConsumption.UUID, {
+        format: Formats.UINT16,
+        unit: 'watts' as Units, // ??
+        maxValue: 100000,
+        minValue: 0,
+        minStep: 1,
+        perms: [Perms.PAIRED_READ, Perms.NOTIFY],
+      });
+    }
+  };
+
+  HAP.TotalConsumption = class TotalConsumption extends HAP.Characteristic {
+    public static readonly UUID: string = 'E863F10C-079E-48FF-8F27-9C2605A29F52';
+
+    constructor() {
+      super('TotalConsumption', TotalConsumption.UUID, {
+        format: Formats.FLOAT,
+        unit: 'kWh' as Units, // ??
+        minValue: 0,
+        minStep: 0.001,
+        perms: [Perms.PAIRED_READ, Perms.NOTIFY],
+      });
+    }
+  };
 
   homebridge.registerPlatform('Comelit', ComelitPlatform);
 }
